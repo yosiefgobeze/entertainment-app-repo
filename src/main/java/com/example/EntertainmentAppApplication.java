c0a8377dfd8d870a4326b5da8bab86d7ca8ee52a
Yosief <yosiefgobeze@gmail.com>
2022-10-29 09:55:15 -0500

    Springboot & swagger: initial commit

---
+++ src/main/java/com/example/EntertainmentAppApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntertainmentAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntertainmentAppApplication.class, args);
+	}
+
+}
